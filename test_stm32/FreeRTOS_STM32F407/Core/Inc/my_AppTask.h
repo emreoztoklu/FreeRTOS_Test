b7870118816e1e79f3a52957763bf3ad6dd8e966
emreoztoklu <emreoztoklu@hotmail.com>
2023-02-15 15:09:47 +0300

    FreeRTOS
    testting

---
+++ test_stm32/FreeRTOS_STM32F407/Core/Inc/my_AppTask.h
@@ -0,0 +1,31 @@
+/*
+ * my_AppTask.h
+ *
+ *  Created on: Feb 7, 2023
+ *      Author: arge12
+ */
+
+#ifndef INC_MY_APPTASK_H_
+#define INC_MY_APPTASK_H_
+
+#include "main.h"
+#include "FreeRTOS.h"
+#include "task.h"
+#include "queue.h"
+
+
+
+
+
+// Idle Hook
+void vApplicationIdleHook(void);
+
+// task prototypes
+void task_100ms(void *param);
+void task_200ms(void *param);
+
+
+
+
+
+#endif /* INC_MY_APPTASK_H_ */
---
+++ test_stm32/FreeRTOS_STM32F407/Core/Src/my_AppTask.c
@@ -0,0 +1,80 @@
+/*
+ * my_AppTask.c
+ *
+ *  Created on: Feb 7, 2023
+ *      Author: arge12
+ */
+
+
+#include "my_AppTask.h"
+
+#define FALSE (1 == 0)
+#define TRUE  (1 == 1)
+#define NOT_PRESSED FALSE
+#define PRESSED TRUE
+
+
+
+const TickType_t xPeriod_100 = pdMS_TO_TICKS(50);
+const TickType_t xPeriod_200 = pdMS_TO_TICKS(30);
+
+
+uint8_t button_status_flag = NOT_PRESSED;
+
+
+
+
+void vApplicationIdleHook(void){
+    // send CPU to normal sleep by using CMSIS macro
+    __WFI();
+}
+
+
+void task_100ms(void *param){
+    TickType_t xLastWakeTime;
+
+    /* Init the xLastWakeTime variable with the current time */
+    xLastWakeTime = xTaskGetTickCount();
+
+    while(1){
+        if(HAL_GPIO_ReadPin(B1_GPIO_Port, B1_Pin) == FALSE){
+            // If button state is 1 then we consider not pressed
+            button_status_flag = NOT_PRESSED;
+        }
+        else{
+            // When button is pressed the state of button will be 0
+            button_status_flag = PRESSED;
+        }
+
+      //  HAL_Delay(50);
+
+        //vTaskDelay(pdMS_TO_TICKS(200));
+        vTaskDelayUntil(&xLastWakeTime, xPeriod_100);
+    }
+}
+
+
+
+void task_200ms(void *param){
+    TickType_t xLastWakeTime;
+
+    /* Init the xLastWakeTime variable with the current time */
+    xLastWakeTime = xTaskGetTickCount();
+
+    while(1) {
+        if(button_status_flag == PRESSED) {
+            // Turn ON the LED
+            HAL_GPIO_WritePin(LD3_GPIO_Port, LD3_Pin|LD4_Pin|LD5_Pin|LD6_Pin, GPIO_PIN_SET);
+        }
+        else {
+            // Turn OFF the LED
+            HAL_GPIO_WritePin(LD3_GPIO_Port, LD3_Pin|LD4_Pin|LD5_Pin|LD6_Pin , GPIO_PIN_RESET);
+        }
+
+        //vTaskDelay(pdMS_TO_TICKS(100));
+        vTaskDelayUntil(&xLastWakeTime, xPeriod_200);
+    }
+}
+
+
+
